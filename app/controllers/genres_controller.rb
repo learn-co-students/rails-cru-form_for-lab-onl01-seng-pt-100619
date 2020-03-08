540252e490f8a7eb0a31b5130445810817f6c87e
Liz Laffitte <liz.kossakowski@gmail.com>
2020-03-08 16:56:14 +0000

    Done.

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,18 +1,27 @@
 class GenresController < ApplicationController
   
     def new 
+      @genre = Genre.new
   end
   
   def create 
+    @genre = Genre.new(obj_params)
+    @genre.save
+    redirect_to genre_path(@genre)
   end 
   
   def show 
+    @genre = Genre.find(params[:id])
   end
   
   def edit 
+    @genre = Genre.find(params[:id])
   end
   
   def update
+    @genre = Genre.find(params[:id])
+    @genre.update(obj_params)
+    redirect_to genre_path(@genre)
   end 
   
   def index 
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,17 +1,28 @@
 class SongsController < ApplicationController
   def new 
+    @song = Song.new
   end
   
   def create 
+    @song = Song.new(obj_params)
+    @song.save
+    redirect_to song_path(@song)
   end 
   
   def show 
+    @song = Song.find(params[:id])
+
   end
   
   def edit 
+    @song = Song.find(params[:id])
+    
   end
   
   def update
+    @song = Song.find(params[:id])
+    @song.update(obj_params)
+    redirect_to song_path(@song)
   end 
   
   def index 
