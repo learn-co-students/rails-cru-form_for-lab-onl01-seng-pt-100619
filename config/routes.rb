7532c3be8c63aeed0f75c6256c620245ed07bd32
wendyv510 <wendyv510@hotmail.com>
2020-03-09 19:15:19 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    get '/artists/:id', to: "artists#show", as: 'artist' 
+    resources :artists, only: [:index, :new, :create, :show, :edit, :update] 
+    resources :genres, only: [:index, :new, :create, :show, :edit, :update] 
+    resources :songs, only: [:index, :new, :create, :show, :edit, :update]  
 end
