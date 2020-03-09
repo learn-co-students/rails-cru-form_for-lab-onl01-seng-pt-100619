064a2f9e2730c374e4f799b944a51813808d0fe4
wendyv510 <wendyv510@hotmail.com>
2020-03-09 19:35:03 -0400

    Done.

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,21 @@
 class GenresController < ApplicationController
+
+    def index 
+        @genres = Genre.all 
+    end 
+
+    def new 
+        @genre = Genre.new 
+    end 
+
+    def show 
+        @genre = Genre.find(params[:id]) 
+    end 
+
+    def create 
+        @genre = Genre.create(params.require(:genre).permit(:name))
+        @genre.save 
+           redirect_to genre_path(@genre) 
+    end 
+    
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,20 @@
 class SongsController < ApplicationController
+    def index 
+        @songs = Song.all 
+    end 
+
+    def new 
+        @song = Song.new
+    end 
+    
+    def show 
+        @song = Song.find(params[:id]) 
+    end 
+
+    def create 
+        @song = Song.create(params.require(:song).permit(:name, :artist_id, :genre_id))
+        @song.save 
+           redirect_to song_path(@song) 
+    end 
+    
 end
