67bad19239b512dd4f0f22b93da8ff78715e0c1d
allisoncortez <allizetorc@gmail.com>
2020-03-10 21:48:29 -0500

    songs MVC

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   resources :artists, only: [:index, :show, :new, :create, :edit, :update]
   resources :genres, only: [:index, :show, :new, :create, :edit, :update]
+  resources :songs, only: [:index, :show, :new, :create, :edit, :update]
   
 
 
